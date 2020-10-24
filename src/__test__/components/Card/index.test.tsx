2d631a3a5037de1d033a3624724ee206c4f7b1ac
githiago-f <tfarias@protonmail.com>
2020-10-23 23:07:01 -0300

    fix: inserting <Router/> to test

--- src/__test__/components/Card/index.test.tsx
+++ src/__test__/components/Card/index.test.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Product from 'components/Product';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('# Card component test', () => {
   const title = 'Hello World!';
 
   beforeEach(() => {
-    render(<Product title={title} />);
+    render(
+      <Router>
+        <Product title={title} />
+      </Router>
+    );
   });
 
-  it('should Render Card component without crashing', () => {
+  it('renders correctly', () => {
     expect(screen).toMatchSnapshot();
   });
 
-  it('Contains small with "Hello World!"', () => {
-    const received = screen.getByText(/Hello/g);
-    expect(received.innerHTML).toBe('Hello World!');
-    expect(received.tagName.toLowerCase()).toBe('small');
+  it('got a Heading component that match RegEx(Hello)', () => {
+    const comp = screen.getByText(/Hello/gi);
+    expect(comp.innerHTML).toBe('Hello World!');
+    expect(comp.tagName).toBe('SMALL');
   });
 });
