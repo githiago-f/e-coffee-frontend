5c81ffc22aba1c7f50f69312196236e66fcb17f0
githiago-f <52945005+githiago-f@users.noreply.github.com>
2021-06-13 15:50:09 +0000

    Deploying to gh-pages from  @ 417299fda5838d86836139379d2675edc381d9ed 🚀

---
+++ precache-manifest.46394782746423ce01a87fc1094a182d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7e8ce55f2233cb8437c59bd78a5190",
+    "url": "/e-coffee-frontend/index.html"
+  },
+  {
+    "revision": "81750362b6971124c529",
+    "url": "/e-coffee-frontend/static/js/0.9d790ab4.chunk.js"
+  },
+  {
+    "revision": "a5434076f47a6533338a",
+    "url": "/e-coffee-frontend/static/js/3.1ab72be8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-coffee-frontend/static/js/3.1ab72be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7b8040c0fd2cd083d9",
+    "url": "/e-coffee-frontend/static/js/main.438dc2a5.chunk.js"
+  },
+  {
+    "revision": "4029e52e4494253a5b7b",
+    "url": "/e-coffee-frontend/static/js/runtime-main.f0bdf008.js"
+  },
+  {
+    "revision": "38dae47ffd489c3c3450d345092d6014",
+    "url": "/e-coffee-frontend/static/media/cart.38dae47f.svg"
+  },
+  {
+    "revision": "95a76351cb7fa9668208f3e90855b41f",
+    "url": "/e-coffee-frontend/static/media/coffee.95a76351.svg"
+  },
+  {
+    "revision": "063d5b241b1816f007a5b1357dc31b82",
+    "url": "/e-coffee-frontend/static/media/down-rating-coffee.063d5b24.svg"
+  },
+  {
+    "revision": "c5b73bfda8ebb9b4191ebb015f5bbab7",
+    "url": "/e-coffee-frontend/static/media/left-chevron.c5b73bfd.svg"
+  },
+  {
+    "revision": "6cea9b537433a2ad77f32d5880b22705",
+    "url": "/e-coffee-frontend/static/media/right-chevron.6cea9b53.svg"
+  },
+  {
+    "revision": "cfb49f55eb8da55ee5e66a7587b12849",
+    "url": "/e-coffee-frontend/static/media/undraw_empty_xct9.cfb49f55.svg"
+  },
+  {
+    "revision": "5696dd5023df2b057a5d2897c671d464",
+    "url": "/e-coffee-frontend/static/media/up-rating-coffee.5696dd50.svg"
+  }
+]);
